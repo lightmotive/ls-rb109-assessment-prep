09881cda9d41820192925d18e3bd53ca7abe82b9
Andre Light <andrelight.al@gmail.com>
2022-02-14 23:39:59 +0000

    Process the problem: [PE]DAC

--- interview_prep_video1_problem2.rb
+++ interview_prep_video1_problem2.rb
@@ -6,8 +6,18 @@
 # For example, if a character occurs 3 times in all strings but not 4 times,
 # you need to include that character three times in the final answer.
 
+# - Input: Array of lowercase strings
+# - Output: Array of characters, including duplicates, that show up in all
+#   strings.
+#
+# Mental model: Find all characters that repeat across all strings. If a
+#   character repeats multiple times across all strings, include those
+#   globally duplicated repetitions.
+
 # Examples:
 p common_chars(%w[bella label roller]) == %w[e l l]
 p common_chars(%w[cool lock cook]) == %w[c o]
 p common_chars(%w[hello goodbye booya random]) == %w[o]
 p common_chars(%w[aabbaaaa ccdddddd eeffee ggrrrrr yyyzzz]) == []
+p common_chars(%w[aa aa aa aa]) == %w[a a]
+p common_chars(%w[aa aa aa aaa]) == %w[a a]
