eb0858324660f243c916403c87f8b07711fd5cfe
Andre Light <andrelight.al@gmail.com>
2022-02-15 18:14:03 +0000

    Initialize interview 2, problem 1

---
+++ 02_interview2_problem1.rb
@@ -0,0 +1,18 @@
+# You have to create a method that takes a positive integer number and returns
+# the next bigger number formed by the same digits:
+# 12 ==> 21
+# 513 ==> 531
+# 2017 ==> 2071
+
+# If no bigger number can be composed using those digits, return -1:
+# 9 ==> -1
+# 111 ==> -1
+# 531 ==> -1
+
+p next_bigger_num(9) == -1
+p next_bigger_num(12) == 21
+p next_bigger_num(513) == 531
+p next_bigger_num(2017) == 2071
+p next_bigger_num(111) == -1
+p next_bigger_num(531) == -1
+p next_bigger_num(123_456_789) == 123_456_798
